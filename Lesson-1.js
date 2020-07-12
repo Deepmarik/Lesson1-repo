1c728b8b60547dd745001c49805d9b4e445bf506
Deepmarik <deepmarik@gmail.com>
2020-07-12 13:33:26 +0300

    part 1

--- Lesson-1.js
+++ Lesson-1.js
@@ -1,4 +1,9 @@
-use strict;
-let grad=prompt("Gradus: ")
-prompt("Введите температуру в градусах Цельсия: ")
-alert("hello")
+"use strict";
+alert("hello!");
+let grd=0;
+let Tf=0;
+grd = parseInt(prompt("Введите занчение температуры в градусах Цельсия: "));
+Tf = (9 / 5) * grd + 32;
+//alert(grd);
+alert( "В фарегейтах это-"+Tf);
+//console.log(333);
